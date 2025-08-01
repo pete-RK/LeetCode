2168cd0f2dd21f24f054f2b1d313e3231368b735
pete-RK <158525501+pete-RK@users.noreply.github.com>
2025-07-31 23:20:56 -0700

    LC-count-total-number-of-colored-cells [Runtime: 0 ms; Memory: 17.5 MB]

--- math/count-total-number-of-colored-cells.py
+++ math/count-total-number-of-colored-cells.py
@@ -1,10 +1,3 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        total = 0
-
-        for i in range(n):
-            total += (2 ** i + 1)* 2
-        
-        total -= (2**n +1)
-
-        return total
+        return 1 + 4 * n * (n-1) // 2
